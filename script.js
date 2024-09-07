e540316c36fc6c0c3ce0f34ac8f232d5e71ed7a7
ansalox <sanchezcode7@gmail.com>
2024-09-07 11:43:18 -0500

    First commit

---
+++ script.js
@@ -0,0 +1,30 @@
+
+// Alerta que sale en los navegadores
+// alert("Estoy dentro del archivo JS")
+
+//confirm("Deseas salir de la pagina")
+
+// prompt("Ingresa tu nombre")
+
+// Consola del navegador
+//console.log("Esto es un texto en log")
+
+//console.error("error de codigo")
+
+//console.warn("Faltaron datos")
+
+//console.info("Inicia el codigo")
+
+// var userName = "juanito"
+// var userId = "1244556"
+// var age = 26
+
+let userName = "juanito"
+const userId = "1244556"
+let age = 26
+
+ userName = "benito"
+ 
+
+console.log("El nombre es", userName, "La cedula es", userId, "su edad es", age)
+
